8586f7490e3c58d416e10c5a2dc464529637301f
Bittersweet <zhouziqun@cool2645.com>
2020-04-30 20:05:48 -0700

    :sparkles: tweak Makefile to mount ext2 in archlinux

--- code/chapt4/init/entry.c
+++ code/chapt4/init/entry.c
@@ -41,6 +41,16 @@ int kern_entry()
 	console_clear();
 
 	console_write_color("Hello, OS kernel!\n", rc_black, rc_green);
+    
+    /*char testStr[2020];
+    testStr[2019] = '\0';
+    int i = 0;
+    for (i = 0; i < 2019; i++)
+    {
+        testStr[i] = 'a' + (i % 26);
+    }
+    console_write(testStr);
+    */
 
 	return 0;
 }
